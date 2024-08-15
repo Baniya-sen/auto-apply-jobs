31ef136b92b1c63789ed84145dcdc022cd074d90
Baniya-sen <144620117+Baniya-sen@users.noreply.github.com>
2024-08-15 21:01:57 +0530

    Update answer_form_filler.py

--- answer_form_filler.py
+++ answer_form_filler.py
@@ -1,41 +1,50 @@
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import Select
 from selenium.common.exceptions import TimeoutException, NoSuchElementException, InvalidSelectorException
 
 
 class FillAnswers:
-    def __init__(self, driver, nlp_output, questions):
+    def __init__(self, driver, questions, answers):
         self.driver = driver
-        self.nlp_output = nlp_output
         self.questions = questions
+        self.answers = answers
 
     def fill_answers(self):
-        for answer, question in zip(self.nlp_output, self.questions):
+        for answer, question in zip(self.answers, self.questions):
             input_type = question['type']
             question_text = question['question']
-            q_text_len = int(len(question_text) * 0.60)
+            ques_text_match_len = min(int(len(question_text) * 0.60), 50)
 
             try:
+
                 if input_type == 'text':
                     input_element = self.driver.find_element(
                         By.XPATH, f"//label[contains(normalize-space(text()), '"
-                        f"{question_text.strip()}')]/following-sibling::input")
+                                  f"{question_text.strip()}')]/following-sibling::input")
+                    input_element.clear()
                     input_element.send_keys(answer['text'])
 
                 elif input_type == 'radio':
                     radio_xpath = (f"//legend[starts-with(normalize-space(), '"
-                                   f"{question_text[:q_text_len]}')]/following::label"
-                                   f"[text()='{answer['radio']}']")
+                                   f"{question_text[:ques_text_match_len]}')]/following::"
+                                   f"label[text()='{answer['radio']}']")
                     radio_element = self.driver.find_element(By.XPATH, radio_xpath)
                     radio_element.click()
 
                 elif input_type == 'select':
-                    select_xpath = (f"//legend[starts-with(normalize-space(), '"
-                                    f"{question_text[:q_text_len]}')]/following-"
-                                    f"sibling::select")
+                    select_xpath = (f"//label[starts-with(normalize-space(), '"
+                                    f"{question_text[:ques_text_match_len]}')]"
+                                    f"/following-sibling::select")
                     select_element = self.driver.find_element(By.XPATH, select_xpath)
-                    select_element.select_by_visible_text(answer['select'])
+                    select_object = Select(select_element)
+                    select_object.select_by_visible_text(answer['select'])
 
-            except (NoSuchElementException, TimeoutException, InvalidSelectorException) as e:
+                elif input_type == 'checkbox':
+                    checkbox_xpath = (f"//legend[starts-with(normalize-space(), '"
+                                      f"{question_text[:20]}')]/following::"
+                                      f"label")
+                    checkbox_element = self.driver.find_element(By.XPATH, checkbox_xpath)
+                    checkbox_element.click()
+
+            except (TimeoutException, NoSuchElementException, InvalidSelectorException) as e:
                 print(f"Failed to fill {input_type} for question '{question_text}': {str(e)}")

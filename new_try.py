8fba4aa54e756eaf8201bba9c302b7a0485dd50f
Baniya-sen <144620117+Baniya-sen@users.noreply.github.com>
2024-11-05 21:09:53 +0530

    Delete new_try.py

--- new_try.py
+++
@@ -1,7 +0,0 @@
-DEFAULT_LINK = "https://www.naukri.com/mnjuser/recommendedjobs"
-link = "https://www.naukri.com/mnjuser/recommendedjobs/12"
-
-jobs_css_pass = ("div.recommended-jobs-page div.list article"
-                 if link == DEFAULT_LINK else "article")
-
-print(jobs_css_pass)

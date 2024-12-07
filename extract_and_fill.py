cbabb37069bb24980d5c5fb7bc4d0e5dba0b57cd
Baniya-sen <144620117+Baniya-sen@users.noreply.github.com>
2024-12-07 18:01:39 +0530

    Update extract_and_fill.py

--- extract_and_fill.py
+++ extract_and_fill.py
@@ -5,6 +5,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.remote.webdriver import WebDriver
+from selenium.webdriver.remote.webelement import WebElement
 from selenium.webdriver.support import expected_conditions as ec
 from selenium.common.exceptions import TimeoutException, NoSuchElementException
 

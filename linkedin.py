c61d36f0395a9567315be255856720025e7140cd
Baniya-sen <144620117+Baniya-sen@users.noreply.github.com>
2024-12-21 13:05:55 +0530

    Update linkedin.py

--- linkedin.py
+++ linkedin.py
@@ -1,6 +1,7 @@
 import time
 
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.remote.webdriver import WebDriver
 from selenium.webdriver.support import expected_conditions as ec
 from selenium.webdriver.support.ui import WebDriverWait
